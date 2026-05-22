58699236ed1af4d187bba6fc704481057ad0c255
vera <pvk@cds.guru>
2026-05-22 10:40:04 +0300

    Use render port env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 import { app } from './app';
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
